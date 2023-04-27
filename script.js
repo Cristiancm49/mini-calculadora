66ad62cf93928556b6428327189720d550c1964c
ccortesm <ccortesm2000@gmail.com>
2023-04-27 17:40:40 -0500

    Se agrego las funciones a cada boton de su respectiva funcion

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const valor1 = document.querySelector('.valor1');
-const valor2 = document.querySelector('.valor2');
+const valor1 = document.querySelector('#valor1');
+const valor2 = document.querySelector('#valor2');
 const respuesta = document.querySelector('.respuesta')
 const btnsumar = document.querySelector('.btn-sumar')
+const btnrestar = document.querySelector('.btn-restar')
+const btndividir = document.querySelector('.btn-dividir')
+const btnmultiplicar = document.querySelector('.btn-multiplicar')
+
+btnsumar.addEventListener("click", sumarNumeros);
+btnmultiplicar.addEventListener('click', multiplicarNumeros);
+btndividir.addEventListener('click', dividirNumeros);
+btnrestar.addEventListener('click', restarNumeros);
 
 
 
@@ -9,4 +17,19 @@ function sumarNumeros() {
     const resultadosuma = parseInt(valor1.value) + parseInt(valor2.value);
     respuesta.innerText = resultadosuma;
 
+}
+
+function restarNumeros () {
+    const resultadoResta = parseInt(valor1.value) - parseInt(valor2.value);
+    respuesta.innerText = resultadoResta;
+}
+
+function multiplicarNumeros () {
+    const resultadoMultiplicar = parseInt(valor1.value) * parseInt(valor2.value);
+    respuesta.innerText = resultadoMultiplicar;
+}
+
+function dividirNumeros () {
+    const resultadoDividir = parseInt(valor1.value) / parseInt(valor2.value);
+    respuesta.innerText = resultadoDividir;
 }
